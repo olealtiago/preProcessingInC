86d9f80261a79f2e4ce69400df8dd336b0e748c1
olealtiago <tiagolucasleal570@gmail.com>
2022-08-03 10:58:48 -0300

    feature: add new file preProcessing.c

---
+++ preProcessing.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define SCALE_CM_TO_METER_FACTOR                 100
+
+#define HEIGHT_SQUARE_METERS(heightCmInt)        (heightCmInt*heightCmInt / SCALE_CM_TO_METER_FACTOR) 
+
+
+#define CALCULATE_IMC(weightKgFloat, heightCmInt) \
+		( (float) (HEIGHT_SQUARE_METERS(heightCmInt))/weightKgFloat ) //typeCast
+
+int main(){
+	int height   = 182;
+	float weight = 72.5;
+	float imc    = 0.0;
+
+	imc = CALCULATE_IMC(weight, height);
+	printf("IMC: %f\n", imc);
+return 0;
+}
